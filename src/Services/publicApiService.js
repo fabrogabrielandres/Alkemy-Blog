4addb22b37db15d450588238355d0ace55d4f65a
jtomasi <juan.tomasi@alkemy.org>
2021-07-24 10:23:47 -0300

    Revert "ong-template-v2"
    This reverts commit f96a7c8d67aac45dbf1ecdfc0e7edb6479c3358a.

--- src/Services/publicApiService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get
