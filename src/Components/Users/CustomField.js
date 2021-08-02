a4bac854c467357420e76f9dc83832c9e1bc1545
Daniel Montalva <danieleduardomontalva@gmail.com>
2021-08-02 08:52:17 +0000

    Merged in feature/OT53-34-formulario-edición---creación-de (pull request #13)
    Feature/OT53-34 formulario edición   creación de
    
    * formik custom inputs and yup validation
    
    * select field
    
    * file input and textarea
    
    * schema module - axios put and post
    
    * some comments
    
    * API url fixed
    
    
    Approved-by: Pablo Gotelli

---
+++ src/Components/Users/CustomField.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useField } from 'formik';
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage
+  } from "@chakra-ui/react";
+
+const CustomField = ({label, fieldType, ...props }) => {
+
+  
+  const [field, meta, helpers] = useField(props);
+  
+  //fieldType is the actual input imported from chakra 
+  //which is displayed as a Field tag dinamically
+  const Field = fieldType;
+    return (
+      <FormControl isInvalid={meta.touched && meta.error}>
+        <FormLabel htmlFor={props.id || props.name}>{label}</FormLabel>
+            <Field {...field} {...helpers} {...props}>
+        
+            </Field>
+        <FormErrorMessage>{meta.error}</FormErrorMessage>
+      </FormControl>
+    );
+  };
+
+export default CustomField
---
+++ src/Components/Users/UserForm.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import {
+    Button,
+    Select,
+    Container,
+    Input,
+    Textarea
+} from "@chakra-ui/react";
+import CustomField from './CustomField';
+import axios from 'axios';
+import { SignupSchema } from './ValidationSchema';
+
+export const UserForm = ({user = {}}) => {
+
+    //"user" values will be used to make the form behave as an edit form.
+    //If this component is used as a child from a parent which provide
+    //user values, those values should be at least the values listed on
+    //Formik's "initialValues" list to work properly.
+
+    //Default: user={} which means the form will behave as a "create" form.
+
+    //Some form inputs like "id" or "password" will render only if there is
+    //somethin into "user.name"
+
+    return(
+
+    <div>
+        <h1>{user.name ? "Editar" : "Crear"}</h1>
+
+        <Formik
+
+            initialValues={{
+                name: user.name || '',
+                email: user.email || '',
+                password: user.password || '',
+                role_id: user.role_id || '',
+                profilePhoto: '',
+                description: user.description || ''
+            }}
+
+            validationSchema={SignupSchema}
+
+            onSubmit={ (values) => {
+
+                if(user.name) {
+                    //Also, the method will be different 
+                    //if there is somethin into "user.name"
+                    //Doing a "put" method to edit or a "post" to create
+                    console.log(values)
+                    axios.put(`http://ongapi.alkemy.org/api/users/${user.id}#t53`, values)
+                    .then(response => console.log(response))
+                    .catch(err => console.log(err))
+            
+                } else {
+                    axios.post('http://ongapi.alkemy.org/api/users#t53/', values)
+                    .then(response => console.log(response))
+                    .catch(err => console.log(err))  
+                }
+            }}
+        >
+
+        {() => (
+            <Form>
+                    <Container>
+
+                        {user.name ? 
+                            <CustomField 
+                                fieldType={Input} 
+                                name='id' 
+                                type='number' 
+                                label='ID' 
+                                value={user.id} 
+                                diabled
+                            /> 
+                                 
+                        : null}
+
+                        <CustomField fieldType={Input} name="name" label="Nombre"/>
+                        
+                        <CustomField fieldType={Input} name="email" type="email" label="Email"/>
+                        
+                        {!user.name ? 
+                            <CustomField 
+                                fieldType={Input} 
+                                name='password' 
+                                type='password' 
+                                label='Contraseña' 
+                            /> 
+                                 
+                        : null}
+
+                        <CustomField fieldType={Select} name="role_id" placeholder='Selección de rol' label="Rol">
+                            <option value="1">Administrador</option>
+                            <option value="2">Usuario</option>
+                        </CustomField>
+                        
+                        <CustomField fieldType={Input} name='profilePhoto' type='file' label="Foto de perfil"/>
+
+                        <CustomField fieldType={Textarea} name ='description' label="Comentarios"/>
+
+                        <CustomField fieldType={Button} name='button' type="submit">Enviar</CustomField>
+
+                    </Container>
+            </Form>
+        )}
+        </Formik>
+    </div>
+)
+};
