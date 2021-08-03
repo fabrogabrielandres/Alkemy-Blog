c2d2a1630154700e7a7e70543aa62325b061d8a6
FabroGabriel <fabrogabrielandres@gmail.com>
2021-08-02 22:57:37 -0500

    se arreglo conflictos

--- src/App.js
+++ src/App.js
@@ -5,21 +5,11 @@ import Router from './router/Router';
 
 function App() {
   return (
-<<<<<<< HEAD
-
-    <div className="App">
-
-      <ChakraProvider>
-        <Router />
-      </ChakraProvider>
-    </div>
-=======
     <ChakraProvider>
       <div className="App">
         <Router />
       </div>
     </ChakraProvider>
->>>>>>> 9ead7674f1b0d1224c9552f00a7fed9165ca45b8
   );
 }
 
