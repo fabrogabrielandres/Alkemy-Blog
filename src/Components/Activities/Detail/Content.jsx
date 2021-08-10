5350b043d34241b9d8f0b3394c2fbd2b5f61c3cd
Nicolas Di Pietro Paolo <dipietronicolas@gmail.com>
2021-08-10 05:04:48 +0000

    Merged in feature/OT53-74-crear-componente-contenido-de-ac (pull request #44)
    Contenido de actividad
    
    * Contenido de actividad
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t53-project into feature/OT53-74-crear-componente-contenido-de-ac
    
    
    Approved-by: Pablo Gotelli

---
+++ src/Components/Activities/Detail/Content.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+import { Flex } from '@chakra-ui/react';
+
+export const Content = ({ content }) => {
+
+  return (
+    <Flex 
+      p={6} 
+      flexDirection="column" 
+      alignItems="center" 
+      mx="auto"
+      w={['90%', '80%', '70%']}
+      fontSize="1.25rem">
+      { 
+        ReactHtmlParser(content) 
+      }
+    </Flex>
+  )
+}
--- src/Components/Activities/Detail/index.jsx
+++ src/Components/Activities/Detail/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import Title from '../../Title/Title';
+import { Content } from './Content';
 
 import {
   Box
 } from '@chakra-ui/react';
+
+const testingContent = `
+  <h3>Titulo de la actividad</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+  nisi ut aliquip ex ea commodo consequat.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+  nisi ut aliquip ex ea commodo consequat.</p>
+`
 
 const Detail = ({ content }) => {
   return (
@@ -14,6 +27,7 @@ const Detail = ({ content }) => {
       <Box w="100%" p={4} color="black" mt='0' mb='20px' fontSize='3rem'>
         {content}
       </Box>
+      <Content content={testingContent} />
     </>
   );
 }
