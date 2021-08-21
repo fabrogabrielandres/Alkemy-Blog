3b39a66e97b6b189918d29cf5b0cf6fcbcd3d463
fabro gabriel <fabrogabrielandres@gmail.com>
2021-08-21 21:21:16 +0000

    Merged in feature/OT53-117-integrar-mercado-pago-en-screen (pull request #82)
    Terminado
    
    * Terminado
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t53-project into feature/OT53-117-integrar-mercado-pago-en-screen
    
    
    Approved-by: Pablo Gotelli

--- src/Components/Donations/MercadoPago.js
+++ src/Components/Donations/MercadoPago.js
@@ -1,12 +1,18 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
+import ScriptTag from 'react-script-tag';
 
 const MercadoPago = () => {
     return (
-      <>
+      <Box>
       <button style={{height:50, width:50}} id="close-image">
         <img alt="" src="https://img.icons8.com/color/50/000000/mercado-pago.png" />
+        <ScriptTag
+          data-preference-id="210986520-b9ded2d8-540a-44a2-b606-207e3b8c7758"
+          src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+        />
       </button>
-      </>
+      </Box>
     );
   }
   
